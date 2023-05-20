0a319ae69d8a279b2fe1b8e076e6fc44004883d9
erener <erener@DESKTOP-KPUBUN9>
2023-05-20 19:58:44 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Cinsiyetiniz Nedir");
+            string cinsiyet = Convert.ToString(Console.ReadLine());
+            switch (cinsiyet)
+            {
+                case erkek:
+                    break;
+                case kadın:
+                    break;
+                default:
+                    Console.WriteLine("Hata");
+                    break;
+                    
+                        if (cinsiyet = erkek)
+                            
+            }
+            Console.WriteLine("Yaşınızı girin");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age > 60)
+            {
+                Console.WriteLine("Maaşınız nedir");
+                int maaş = Convert.ToInt32(Console.ReadLine());
+                int yaş = maaş * 10;
+                Console.WriteLine((maaş * 10));
+            }
+            else
+            {
+                Console.WriteLine("Kaç gün çalıştınız");
+                int gün = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Maaşınız nedir");
+                int maaş = Convert.ToInt32(Console.ReadLine());
+                int yaş = maaş = maaş * 11;
+                Console.WriteLine((maaş * 11));
+            }
+            if (cinsiyet = kadın)
+
+
+
+                Console.WriteLine("Yaşınızı girin");
+            int AGE = Convert.ToInt32(Console.ReadLine());
+            if (AGE > 58)
+            {
+                Console.WriteLine("Maaşınız nedir");
+                int maaş = Convert.ToInt32(Console.ReadLine());
+                int yaş = maaş * 10;
+                Console.WriteLine((maaş * 10));
+            }
+            else
+            {
+                Console.WriteLine("Kaç gün çalıştınız");
+                int gün = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Maaşınız nedir");
+                int maaş = Convert.ToInt32(Console.ReadLine());
+                int yaş = maaş = maaş * 11;
+                Console.WriteLine((maaş * 11));
+            }
+                Console.WriteLine("Tebrikler Emekli oldunuz")
+
+
+
+
+
+            }
+        }
+    }
+}
